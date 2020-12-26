a8e8c0b810d4afc1a2499c6a6bdcb311096156ef
akhsassoualid <oualid.akhsass@gmail.com>
2020-12-26 15:50:50 +0100

    changes in app file

--- app.py
+++ app.py
@@ -5,30 +5,8 @@ import json
 from datetime import datetime
 from preprocessing import general_process
 
-# Import the json dataset
-data = pd.read_json('News_Category_Dataset_v2.json', lines=True)
-data['date'] = pd.to_datetime(data.date)
-data['year'] = [x.year for x in data['date']]
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
 
-# Let's verify duplicates existance
-duplica = data[data.duplicated('headline')==True]
-data = data[data.duplicated('headline')==False]
-
-# Subset of data for the years of 2018
-data = data[data['year'] >= 2018]
-data = data.reset_index(drop=True)
-headline_data = data
-
-# preprocessing
-headline_data = general_process(data = headline_data, column_headline="headline", new_column_headline="headline_")
-
-# Recommending
-## Title 
-st.title("Articles Headline Recommnder Engine")
-## Image
-#img =Image.open('Headlines_image.jpg')
-#st.image(img, caption='www.depositphotos.com')
-## Headline askd from a reader
-st.header('Choose an article that you heave read')
-st.subheader("You can choose from the headlines of the following link \n https://drive.google.com/file/d/1mHDG0yMs9leCjHcr6h8FX5V10EpUqBIU/view?usp=sharing")
-head_line = st.text_input('copy the headline you have just read to suggest you the similar ones')
+st.write('You selected:', option)
