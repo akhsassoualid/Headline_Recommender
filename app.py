f874abc7bb51f968e89a5c145505a372ae041cd8
akhsassoualid <oualid.akhsass@gmail.com>
2020-12-26 15:27:07 +0100

    changes on app file

--- app.py
+++ app.py
@@ -21,4 +21,14 @@ headline_data = data
 
 # preprocessing
 headline_data = general_process(data = headline_data, column_headline="headline", new_column_headline="headline_")
-st.dataframe(headline_data)
+
+# Recommending
+## Title 
+st.title("Articles Headline Recommnder Engine")
+## Image
+img =Image.open('Headlines_image.jpg')
+st.image(img, caption='www.depositphotos.com')
+## Headline askd from a reader
+st.header('Choose an article that you heave read')
+st.subheader("You can choose from the headlines of the following link \n https://drive.google.com/file/d/1mHDG0yMs9leCjHcr6h8FX5V10EpUqBIU/view?usp=sharing")
+head_line = st.text_input('copy the headline you have just read to suggest you the similar ones')
