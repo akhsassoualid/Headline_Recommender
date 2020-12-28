202b6e66bc4a85e2eee88a18ca6f91f3184f1e05
akhsassoualid <oualid.akhsass@gmail.com>
2020-12-28 15:35:39 +0100

    changes in README and app files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Headline_Recommender
-The project develop and application that suggest to reader more similar articles to the those they alread read. It uses the embedding algorithms of headlines to create their own numerical representation, which allows to compute similarity between headlines and get the most similar ones.
+# A Recommender Engine for headlines articles based on headlines embedding.
+The project develop and application that suggest to reader more similar articles to the those they already read. It uses the embedding algorithms of headlines to create their own numerical representation, which allows to compute similarity between headlines and get the most similar ones.
+
+For purpose of simplicity, we was satisfied with only headlines that concernes the year of 2018.
+
+# Steps of the project
+We build the function "general_process" saved in the preprocessing.py file, to prepare the text data. Its output is the processed_data csv file, that contains the headlines after the preprocessing.
+
+three algorithms are used to build a numerical representation of each headline, We talk about:
+ - NMF and LDA factorization: We create a sparse matrix that composed of rows that represent each headlines and columns that represent each word in the entire vocabulary.
+ - word2vec : A deeplearning approach, that uses an average word2vec of words composing that headline.
+ 
+
--- app.py
+++ app.py
@@ -5,49 +5,72 @@ import json
 from recommender import recommender_engine
 from datetime import datetime
 from preprocessing import general_process
+from PIL import Image
 
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
 
-# Import the json dataset
-data = pd.read_json('News_Category_Dataset_v2.json', lines=True)
-data['date'] = pd.to_datetime(data.date)
-data['year'] = [x.year for x in data['date']]
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 
-# Let's verify duplicates existance
-duplica = data[data.duplicated('headline')==True]
-data = data[data.duplicated('headline')==False]
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
 
-# Subset of headlines related to the year 2018
-data = data[data['year'] >= 2018]
-data = data.reset_index(drop=True)
-headline_data = data
+left_column, right_column = st.beta_columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
 
-#We list in the toolbox all the headlines
-tup = *(headline_data['headline'][i] for i in range(len(headline_data))),
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
 
-# preprocessing of headlines
-headline_data = general_process(data = headline_data, column_headline="headline", new_column_headline="headline_")
+# Import the processed dataset
+headline_data = pd.read_csv('processed_data.csv')
 
-# Recommending
+# We list in the toolbox all the headlines
+tup0 = *(headline_data['headline'][i] for i in range(len(headline_data))),
+tup1 = (" ",)
+tup = tup0 + tup1
+
+# Design of the App
 ## Title 
 st.title("Articles Headline Recommnder Engine")
+
+## Add background image
+from PIL import Image
+image = Image.open('Headlines_image.jpg')
+st.image(image, caption='https://www.wallpaperflare.com', use_column_width=True)
+
 ## Sub title
 st.header('Choose an article that you heave read')
 head_line = st.selectbox('Choose a headline',tup)
-st.write('You selected:', head_line)
+#st.write('You selected:', head_line)
 
 # Get the index of the chosen headline
 index_head = headline_data[headline_data["headline"]==head_line].index[0]
 
-# Toolbox to select a model
+## Toolbox to select a model
 MODEL = st.selectbox(
     'Choose the model you want to use for embedding headlines',
     ('nmf', 'lda', 'word2vec'))
-st.write('You selected:', MODEL)
+#st.write('You selected:', MODEL)
 
-# Toolbox to select the number of suggestion wanted
+## Toolbox to select the number of suggestion wanted
 num_suggestions = st.number_input('Insert and integer number of suggestions you want',min_value=1)
-st.write('The current number is ', num_suggestions)
+#st.write('The current number is ', num_suggestions)
+
+# preprocessing of headlines
+#headline_data = general_process(data = headline_data, column_headline="headline", new_column_headline="headline_")
 
 # Display the suggestions
-if st.button("Search for recommended articles headlines"):
+if st.button("Search for recommendions"):
     st.dataframe(recommender_engine(data=headline_data, index=index_head, n_similar_article=num_suggestions, model=MODEL))
