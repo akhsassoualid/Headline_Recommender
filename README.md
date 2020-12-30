bae9611a32ec196da23a87988c4dc43e369a10a0
akhsassoualid <oualid.akhsass@gmail.com>
2020-12-30 15:30:08 +0100

    Changes in app file and readme and reorganization of folders

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# A Recommender Engine for headlines articles based on headlines embedding.
+# A Recommender Engine for headlines articles using embedded words.
 The project develop and application that suggest to reader more similar articles to the those they already read. It uses the embedding algorithms of headlines to create their own numerical representation, which allows to compute similarity between headlines and get the most similar ones.
 
-For purpose of simplicity, we was satisfied with only headlines that concernes the year of 2018.
+For purpose of simplicity, we was satisfied only with headlines that concernes the year of 2018.
 
 # Steps of the project
 We build the function "general_process" saved in the preprocessing.py file, to prepare the text data. Its output is the processed_data csv file, that contains the headlines after the preprocessing.
@@ -9,5 +9,26 @@ We build the function "general_process" saved in the preprocessing.py file, to p
 three algorithms are used to build a numerical representation of each headline, We talk about:
  - NMF and LDA factorization: We create a sparse matrix that composed of rows that represent each headlines and columns that represent each word in the entire vocabulary.
  - word2vec : A deeplearning approach, that uses an average word2vec of words composing that headline.
- 
+those algorithms are exploited with the function "recommender_engine" developed in the recommender py file.
+
+# To excecute the app
+Clone the repository in the commend line using the link : https://github.com/akhsassoualid/Headline_Recommender.
+```
+git clone https://github.com/akhsassoualid/Headline_Recommender.git
+
+```
+
+Install the necessary requirements : 
+```
+pip install -r requirements.txt
+
+```
+Run the application savec in the app.py file
+
+```
+streamlit run app.py
+
+```
+
+# Illustration of the app
 
--- app.py
+++ app.py
@@ -2,75 +2,70 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 import json
+import seaborn as sns
+import matplotlib.pyplot as plt
 from recommender import recommender_engine
 from datetime import datetime
 from preprocessing import general_process
+from distribution_plots import distr_plot_by
 from PIL import Image
-
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
-
-left_column, right_column = st.beta_columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
-
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
-
-# Import the processed dataset
+# -----------------------------------------------------------------------------------
+# Import the processed dataset and orginal data
 headline_data = pd.read_csv('processed_data.csv')
+data = pd.read_json('News_Category_Dataset_v2.json', lines=True)
 
+# Design of the App
+## Title 
 # We list in the toolbox all the headlines
 tup0 = *(headline_data['headline'][i] for i in range(len(headline_data))),
 tup1 = (" ",)
 tup = tup0 + tup1
 
-# Design of the App
-## Title 
 st.title("Articles Headline Recommnder Engine")
 
 ## Add background image
-from PIL import Image
 image = Image.open('Headlines_image.jpg')
 st.image(image, caption='https://www.wallpaperflare.com', use_column_width=True)
 
-## Sub title
-st.header('Choose an article that you heave read')
-head_line = st.selectbox('Choose a headline',tup)
-#st.write('You selected:', head_line)
+# -----------------------------------------------------------------------------------
+# Add a selectbox to the sidebar:
+activities = ["EDA Charts","Propose More Headlines"]
+choice = st.sidebar.selectbox('Select an Option',activities)
 
-# Get the index of the chosen headline
-index_head = headline_data[headline_data["headline"]==head_line].index[0]
+if choice == "EDA Charts":
+    st.subheader("EXploratory Charts")
+    # We import the whole data of all years
+    data = pd.read_json('News_Category_Dataset_v2.json', lines=True)
+    data['date'] = pd.to_datetime(data.date)
+    data['year'] = [x.year for x in data['date']]
+    # Display charts
+    yr = st.number_input('Insert a year',min_value=2016)
+    n_data = data[data['year']==yr]
+    fig, ax = plt.subplots(figsize=(20,7))
+    st.write(sns.countplot(x="category", data = n_data, order=n_data["category"].value_counts().index), ax=ax)
+    st.title('Distribution of news catgeory on '+str(yr))
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
-## Toolbox to select a model
-MODEL = st.selectbox(
-    'Choose the model you want to use for embedding headlines',
+
+elif choice == "Propose More Headlines":
+    ## Sub title
+    st.header('Choose an article that you heave read')
+    head_line = st.selectbox('Choose a headline',tup)
+    #st.write('You selected:', head_line)
+
+    ## Toolbox to select a model
+    # Get the index of the chosen headline
+    index_head = headline_data[headline_data["headline"]==head_line].index[0]
+
+    MODEL = st.selectbox('Choose the model you want to use for embedding headlines',
     ('nmf', 'lda', 'word2vec'))
-#st.write('You selected:', MODEL)
+    #st.write('You selected:', MODEL)
 
-## Toolbox to select the number of suggestion wanted
-num_suggestions = st.number_input('Insert and integer number of suggestions you want',min_value=1)
-#st.write('The current number is ', num_suggestions)
-
-# preprocessing of headlines
-#headline_data = general_process(data = headline_data, column_headline="headline", new_column_headline="headline_")
-
-# Display the suggestions
-if st.button("Search for recommendions"):
-    st.dataframe(recommender_engine(data=headline_data, index=index_head, n_similar_article=num_suggestions, model=MODEL))
+    ## Toolbox to select the number of suggestion wanted
+    num_suggestions = st.number_input('Insert and integer number of suggestions you want',min_value=1)
+    #st.write('The current number is ', num_suggestions)
+    
+    # Display the suggestions
+    if st.button("Search for recommendions"):
+        st.dataframe(recommender_engine(data=headline_data, index=index_head, n_similar_article=num_suggestions, model=MODEL))
